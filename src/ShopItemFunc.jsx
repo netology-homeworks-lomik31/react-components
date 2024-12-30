9e799a613b07f34ae82e85c0eb03fd70c0af9ae9
lomik31 <lomik31@codead.dev>
2024-12-31 01:30:26 +0300

    added ShopItemClass + redtructurisation

--- src/ShopItemFunc.jsx
+++ src/ShopItemFunc.jsx
@@ -1,18 +1,19 @@
 function ShopItemFunc(props) {
+    const { item } = props;
     return (
         <div className="main-content">
-            <h2>{props.brand}</h2>
-            <h1>{props.title}</h1>
-            <h3>{props.description}</h3>
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
             <div className="description">
-                {props.descriptionFull}
+                {item.descriptionFull}
             </div>
             <div className="highlight-window mobile">
                 <div className="highlight-overlay"></div>
             </div>
             <div className="divider"></div>
             <div className="purchase-info">
-                <div className="price">{props.currency}{props.price}.00</div>
+                <div className="price">{item.currency}{item.price}.00</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ShopItemFunc from './ShopItemFunc.jsx'
 
-const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-}
+
+
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App>
-            return (
-            <div className="container">
-                <div className="background-element">
-                </div>
-                <div className="highlight-window">
-                <div className='highlight-overlay'></div>
-                </div>
-                <div className="window">
-                <ShopItemFunc item={item} />
-                </div>
-            </div>
-            )
-        </App>
+        <App />
     </StrictMode>,
 )
 
